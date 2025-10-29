046b38df42b4cf0393f3db2b50eb30f7d7a71f1d
JB Ladera <minidomou@gmail.com>
2025-10-29 13:42:45 -0500

    initial java notes

---
+++ HelloJava/src/main/java/HelloJava.java
@@ -0,0 +1,25 @@
+public class HelloJava {
+    // fields,properties
+    // methods,behaviors
+
+    // applications starts at entrypoint - default main method
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        IO.println("this exists");
+
+        // several types in java
+        int a = 34;
+        double b = 345.673;
+
+        // math operators
+        a = 34 - 35;
+
+        // logical operators
+        boolean bal = a > b;
+
+        // control flow
+        // if, while, etc
+
+    }
+}
