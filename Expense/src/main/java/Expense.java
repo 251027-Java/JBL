9d257437f6356d6c465595838c7a4f4669f7665b
JB Ladera <minidomou@gmail.com>
2025-11-03 15:54:22 -0600

    add google coding style

--- Expense/src/main/java/Expense.java
+++ Expense/src/main/java/Expense.java
@@ -2,24 +2,26 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Expense {
-    private int id;
-    private LocalDateTime date;
-    private double value;
-    private String merchant;
 
-    public Expense(int id, LocalDateTime date, double value, String merchant) {
-        this.id = id;
-        this.date = date;
-        this.value = value;
-        this.merchant = merchant;
-    }
+  private int id;
+  private LocalDateTime date;
+  private double value;
+  private String merchant;
 
-    @Override
-    public String toString() {
-        return String.format("Expense [id=%d, date=%s, value=%f, merchant=%s]", id, date, value, merchant);
-    }
+  public Expense(int id, LocalDateTime date, double value, String merchant) {
+    this.id = id;
+    this.date = date;
+    this.value = value;
+    this.merchant = merchant;
+  }
 
-    public String toCSV() {
-        return String.format("%d,%s,%f,%s", id, date, value, merchant);
-    }
+  @Override
+  public String toString() {
+    return String.format("Expense [id=%d, date=%s, value=%f, merchant=%s]", id, date, value,
+        merchant);
+  }
+
+  public String toCSV() {
+    return String.format("%d,%s,%f,%s", id, date, value, merchant);
+  }
 }
--- Expense/src/main/java/Main.java
+++ Expense/src/main/java/Main.java
@@ -1,7 +1,5 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializer;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,31 +9,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    static void main() {
-        List<Expense> list = new ArrayList<>();
 
-        list.add(new Expense(1, LocalDateTime.now(), 99.32, "Walmart"));
-        list.add(new Expense(2, LocalDateTime.now(), 35.2, "Costco"));
-        list.add(new Expense(3, LocalDateTime.now(), 10000, "Private Jet"));
+  static void main() {
+    List<Expense> list = new ArrayList<>();
 
-        System.out.println(list);
+    list.add(new Expense(1, LocalDateTime.now(), 99.32, "Walmart"));
+    list.add(new Expense(2, LocalDateTime.now(), 35.2, "Costco"));
+    list.add(new Expense(3, LocalDateTime.now(), 10000, "Private Jet"));
 
-        try (PrintWriter out = new PrintWriter(new FileWriter("expenses.csv"))) {
-            out.println("id,date,value,merchant");
-            for (var e : list) {
-                out.println(e.toCSV());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    System.out.println(list);
 
-        Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, (JsonSerializer<LocalDateTime>) (src, _, _) -> new JsonPrimitive(src.toString())).setPrettyPrinting().create();
-        String json = gson.toJson(list);
-
-        try (PrintWriter out = new PrintWriter(new FileWriter("expenses.json"))) {
-            out.println(json);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    try (PrintWriter out = new PrintWriter(new FileWriter("expenses.csv"))) {
+      out.println("id,date,value,merchant");
+      for (var e : list) {
+        out.println(e.toCSV());
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+
+    Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class,
+            (JsonDeserializer<LocalDateTime>) (json, _, _) -> LocalDateTime.parse(
+                json.getAsJsonPrimitive().getAsString())).registerTypeAdapter(LocalDateTime.class,
+            (JsonSerializer<LocalDateTime>) (src, _, _) -> new JsonPrimitive(src.toString()))
+        .setPrettyPrinting().create();
+
+    try (FileWriter out = new FileWriter("expenses.json")) {
+      gson.toJson(list, out);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    String json = gson.toJson(list);
+    List<Expense> list2 = gson.fromJson(json, new TypeToken<ArrayList<Expense>>() {
+    });
+
+    System.out.println(list2);
+  }
 }
