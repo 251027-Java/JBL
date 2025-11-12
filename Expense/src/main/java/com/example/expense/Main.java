27d72ac9787c31d96760bf7d3f11b0bf4319f4be
JB Ladera <minidomou@gmail.com>
2025-11-12 14:47:24 -0600

    implement mongo repository (#10)
    * init mongo deps
    
    * finish mongo repository
    
    * optimize imports
    
    * try updating

--- Expense/src/main/java/com/example/expense/Main.java
+++ Expense/src/main/java/com/example/expense/Main.java
@@ -1,13 +1,27 @@
 package com.example.expense;
 
 import com.example.expense.repository.IRepository;
-import com.example.expense.repository.PostgreSQLRepository;
+import com.example.expense.repository.MongoRepository;
 import com.example.expense.service.ExpenseService;
+import com.mongodb.client.model.Filters;
+import java.time.LocalDateTime;
 
 public class Main {
 
     static void main() {
-        IRepository repo = new PostgreSQLRepository();
+        IRepository repo = new MongoRepository();
         ExpenseService service = new ExpenseService(repo);
+
+        System.out.println(service.getExpense(1));
+        System.out.println(service.getExpense(2));
+
+        service.addExpense(34.36, "walmart");
+        service.updateExpense(new Expense(2, LocalDateTime.now(), 100, "walmart"));
+
+        System.out.println(service.getExpense(1));
+        System.out.println(service.getExpense(2));
+
+        var s = Filters.and(Filters.eq("id", 1), Filters.regex("name", "^[0-9]d"));
+        System.out.println(s);
     }
 }
---
+++ Expense/src/main/java/com/example/expense/repository/MongoRepository.java
@@ -0,0 +1,84 @@
+package com.example.expense.repository;
+
+import com.example.expense.Expense;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.Document;
+
+public class MongoRepository implements IRepository {
+
+    private final String user = "mongoadmin";
+    private final String password = "secret";
+    private final String hostname = "localhost";
+    private final int port = 27017;
+
+    private final String uri = String.format("mongodb://%s:%s@%s:%d", user, password, hostname, port);
+
+    private MongoClient client;
+    private final MongoCollection<Document> collection;
+
+    public MongoRepository() {
+        client = MongoClients.create(uri);
+        MongoDatabase db = client.getDatabase("expensesdb");
+        collection = db.getCollection("expense");
+    }
+
+    private static Document toDocument(Expense e) {
+        if (e == null) return null;
+
+        return new Document("_id", e.getId())
+                .append("date", e.getDate())
+                .append("value", e.getValue())
+                .append("merchant", e.getMerchant());
+    }
+
+    private static Expense toExpense(Document e) {
+        if (e == null) return null;
+
+        return new Expense(
+                e.getInteger("_id"),
+                e.getDate("date").toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(),
+                e.getDouble("value"),
+                e.getString("merchant"));
+    }
+
+    @Override
+    public void createExpense(Expense expense) {
+        collection.insertOne(toDocument(expense));
+    }
+
+    @Override
+    public Expense readExpense(int id) {
+        return toExpense(collection.find(Filters.eq("_id", id)).first());
+    }
+
+    @Override
+    public void updateExpense(Expense expense) {
+        collection.updateOne(
+                Filters.eq("_id", expense.getId()),
+                Updates.combine(
+                        Updates.set("date", expense.getDate()),
+                        Updates.set("value", expense.getValue()),
+                        Updates.set("merchant", expense.getMerchant())));
+    }
+
+    @Override
+    public void deleteExpense(int id) {
+        collection.deleteOne(Filters.eq("_id", id));
+    }
+
+    @Override
+    public List<Expense> loadExpenses() {
+        return collection.find().map(MongoRepository::toExpense).into(new ArrayList<>());
+    }
+
+    @Override
+    public void saveExpenses(List<Expense> expenses) {}
+}
