715cc589fa282756eef06617d2738e1e3f0fb643
JB Ladera <minidomou@gmail.com>
2025-11-12 13:49:20 -0600

    optimize imports

--- Expense/src/main/java/com/example/expense/repository/MongoRepository.java
+++ Expense/src/main/java/com/example/expense/repository/MongoRepository.java
@@ -7,7 +7,6 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
