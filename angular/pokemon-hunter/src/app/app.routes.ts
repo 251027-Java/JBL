6a767e4d8c0210e3bbbfd3bb1315206d1eeba278
JB Ladera <minidomou@gmail.com>
2025-11-26 11:56:52 -0600

    lazy loaded routes

--- angular/pokemon-hunter/src/app/app.routes.ts
+++ angular/pokemon-hunter/src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import type { Routes } from "@angular/router";
-import { Catch } from "./components/catch/catch";
-import { Dashboard } from "./components/dashboard/dashboard";
 import { Login } from "./components/login/login";
 import { authGuard } from "./guards/auth-guard";
 
 export const routes: Routes = [
 	{ path: "", component: Login },
-	{ path: "dashboard", component: Dashboard, canActivate: [authGuard] },
-	{ path: "catch", component: Catch, canActivate: [authGuard] },
+	{
+		path: "dashboard",
+		loadComponent: () =>
+			import("./components/dashboard/dashboard").then((e) => e.Dashboard),
+		canActivate: [authGuard],
+	},
+	{
+		path: "catch",
+		loadComponent: () =>
+			import("./components/catch/catch").then((e) => e.Catch),
+		canActivate: [authGuard],
+	},
 ];
