2069350d6626643095327402c9329bacf007cd33
JB Ladera <minidomou@gmail.com>
2025-11-26 10:25:23 -0600

    display pokemon on dashboard

--- angular/pokemon-hunter/src/app/components/dashboard/dashboard.ts
+++ angular/pokemon-hunter/src/app/components/dashboard/dashboard.ts
@@ -1,9 +1,13 @@
-import { Component } from "@angular/core";
+import { TitleCasePipe } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import { PokemonService } from "../../services/pokemon-service";
 
 @Component({
 	selector: "app-dashboard",
-	imports: [],
+	imports: [TitleCasePipe],
 	templateUrl: "./dashboard.html",
 	styleUrl: "./dashboard.css",
 })
-export class Dashboard {}
+export class Dashboard {
+	protected pokemonService = inject(PokemonService);
+}
