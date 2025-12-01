1cfda332201f7c1c4be828a75bb4f4be858f20f0
JB Ladera <minidomou@gmail.com>
2025-12-01 10:11:57 -0600

    init spring project

---
+++ spring/expensereport/src/main/java/com/revature/expensereport/ExpensereportApplication.java
@@ -0,0 +1,13 @@
+package com.revature.expensereport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensereportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensereportApplication.class, args);
+	}
+
+}
