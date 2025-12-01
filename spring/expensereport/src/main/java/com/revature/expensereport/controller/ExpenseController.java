4632ddf083e6fcdc5a5bf8f8cbef7fc7db55775d
JB Ladera <minidomou@gmail.com>
2025-12-01 11:25:53 -0600

    add query params

--- spring/expensereport/src/main/java/com/revature/expensereport/controller/ExpenseController.java
+++ spring/expensereport/src/main/java/com/revature/expensereport/controller/ExpenseController.java
@@ -15,8 +15,8 @@ public class ExpenseController {
     }
 
     @GetMapping("/hello")
-    String hello(@RequestParam String name) {
-        return String.format("Hell %s!", name);
+    String hello(@RequestParam String name, @RequestParam(defaultValue = "Hello") String greeting) {
+        return String.format("%s %s!", greeting, name);
     }
 
     public record Data(String name, int id) {}
