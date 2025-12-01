8834cd5d0d25a64c296a6dbe314a458477950393
JB Ladera <minidomou@gmail.com>
2025-12-01 10:50:52 -0600

    add controller with basic get methods

---
+++ spring/expensereport/src/main/java/com/revature/expensereport/controller/ExpenseController.java
@@ -0,0 +1,23 @@
+package com.revature.expensereport.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class ExpenseController {
+
+    @GetMapping("/test")
+    Data test() {
+        return new Data("this is a test", 0);
+    }
+
+    @GetMapping("/hello")
+    String hello(@RequestParam String name) {
+        return String.format("Hell %s!", name);
+    }
+
+    public record Data(String name, int id) {}
+}
