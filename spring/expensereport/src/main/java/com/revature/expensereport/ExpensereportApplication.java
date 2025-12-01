a8fe226e859f6f411badce6473b0a83d92741e07
JB Ladera <minidomou@gmail.com>
2025-12-01 10:15:56 -0600

    add formatting

--- spring/expensereport/src/main/java/com/revature/expensereport/ExpensereportApplication.java
+++ spring/expensereport/src/main/java/com/revature/expensereport/ExpensereportApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExpensereportApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExpensereportApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ExpensereportApplication.class, args);
+    }
 }
