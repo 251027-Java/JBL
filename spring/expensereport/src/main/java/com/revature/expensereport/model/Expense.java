9660d2be4f4cd83b69a43dd02ee6541dface11f9
JB Ladera <minidomou@gmail.com>
2025-12-01 15:33:49 -0600

    rename column to price

--- spring/expensereport/src/main/java/com/revature/expensereport/model/Expense.java
+++ spring/expensereport/src/main/java/com/revature/expensereport/model/Expense.java
@@ -1,5 +1,6 @@
 package com.revature.expensereport.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -14,6 +15,8 @@ public class Expense {
 
     private String merchant;
     private LocalDateTime date;
+
+    @Column(name = "price")
     private double value;
 
     public Expense() {}
