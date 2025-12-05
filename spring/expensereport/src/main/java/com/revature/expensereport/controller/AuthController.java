34d1c7eed1758c625705076da7cefa41ab6be2da
JB Ladera <minidomou@gmail.com>
2025-12-05 13:59:08 -0600

    implement jwt auth (#25)
    * init jwt implementation
    
    * add validation and generation
    
    * change visibility

---
+++ spring/expensereport/src/main/java/com/revature/expensereport/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.revature.expensereport.controller;
+
+import com.revature.expensereport.repository.UserRepository;
+import com.revature.expensereport.security.JwtUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+record AuthResponse(String token) {}
+
+record AuthRequest(String username, String password) {}
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(JwtUtil jwtUtil, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/login")
+    AuthResponse login(@RequestBody AuthRequest request) {
+        var user = userRepository.findByUsername(request.username());
+
+        if (user.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found");
+        }
+
+        if (!passwordEncoder.matches(request.password(), user.get().getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "invalid username or password");
+        }
+
+        return new AuthResponse(jwtUtil.generateToken(request.username()));
+    }
+}
--- spring/expensereport/src/main/java/com/revature/expensereport/security/WebConfig.java
+++ spring/expensereport/src/main/java/com/revature/expensereport/security/WebConfig.java
@@ -7,13 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final BasicAuthInterceptor basicAuthInterceptor;
+    private final JwtInterceptor jwtInterceptor;
 
-    public WebConfig(BasicAuthInterceptor basicAuthInterceptor) {
+    public WebConfig(BasicAuthInterceptor basicAuthInterceptor, JwtInterceptor jwtInterceptor) {
         this.basicAuthInterceptor = basicAuthInterceptor;
+        this.jwtInterceptor = jwtInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(basicAuthInterceptor).addPathPatterns("/api/**");
+        registry.addInterceptor(jwtInterceptor).addPathPatterns("/api/**").excludePathPatterns("/api/auth/login");
     }
 }
